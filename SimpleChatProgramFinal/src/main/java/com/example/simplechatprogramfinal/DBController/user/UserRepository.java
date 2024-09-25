97a10969af68d241f43ba24c93f367df4d5b1645
Spi0nDK <SpionGamingDK@gmail.com>
2024-09-25 18:32:40 +0200

    Interfaces

--- SimpleChatProgramFinal/src/main/java/com/example/simplechatprogramfinal/DBController/user/UserRepository.java
+++ SimpleChatProgramFinal/src/main/java/com/example/simplechatprogramfinal/DBController/user/UserRepository.java
@@ -3,6 +3,12 @@ package com.example.simplechatprogramfinal.DBController.user;
 import com.example.simplechatprogramfinal.Entity.Users;
 
 public interface UserRepository {
+
+    /**
+     *
+     * @param email Is the users email to find alle the users information
+     * @return The correct user information from the database with the given email
+     */
     Users getUserByEmail(String email);
 
 }
